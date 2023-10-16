a294a90a9f0d0a2234816c73ff7b01647c563e0e
Antonin <piat.antonin02@gmail.com>
2023-10-16 17:31:57 +0200

    commit2

--- TP4/exo4/ajout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Rediriger vers une URL spécifique
-$URL_cible = 'http://localhost/IDAW/TP4/exo4/users.php';
-header($URL_cible);
-exit; // Assure que le script se termine immédiatement après la redirection
-?>
-
--- TP4/exo4/users.php
+++ TP4/exo4/users.php
@@ -27,6 +27,19 @@
 
         $request->execute();
     }
+
+    if(isset($_POST["name-new"])&&isset($_POST["name-new"])){
+        $request = $pdo->prepare("update users SET name = :name , email = :email WHERE id = :id;");
+        $request->bindParam(":name", $_POST["name-new"], PDO::PARAM_STR);
+        $request->bindParam(":email", $_POST["email-new"], PDO::PARAM_STR);
+        $request->bindParam(":id", $_POST["id-new"], PDO::PARAM_STR);
+        $request->execute();
+    }
+
+
+
+
+
     $request = $pdo->prepare("select * from users");
     $request->execute();
     $resultats=$request->fetchall(PDO::FETCH_OBJ);
@@ -38,7 +51,12 @@
 <!DOCTYPE html>
     <html>
     <body>   
-        <table>
+        <table class= "tableau">
+            <thead>
+                <tr>
+                      <th>id</th><th>name</th><th>email</th>
+                </tr>
+            </thead>
             <?php
            
             foreach($resultats as $resultat){
@@ -54,7 +72,9 @@
                     </td>';
                 echo '<td>
                     <form id="ajout_form" action="modify.php" method="POST">
-                    <input type="hidden" name = "cache" value='.$resultat->id.' />
+                    <input type="hidden" name = "id-actuelle" value='.$resultat->id.' />
+                    <input type="hidden" name = "name-actuelle" value='.$resultat->name.' />
+                    <input type="hidden" name = "email-actuelle" value='.$resultat->email.' />
                     <input type="submit" value="Modify">
                     </form>
                     </td>';
@@ -64,18 +84,19 @@
         <br>
         <form id="ajout_form" action="users.php" method="POST">
             <table>
-                <tr>
-                    <th>Login :</th>
-                    <td><input type="text" name="login"></td>
-                </tr>
-                <tr>
-                    <th>Email :</th>
-                    <td><input type="email" name="email"></td>
-                </tr>
-                <tr>
-                    <th></th>
-                    <td><input type="submit" value="add" /></td>
-                </tr>
+                    <tr>
+                        <th>Login :</th>
+                        <td><input type="text" name="login"></td>
+                    </tr>
+                    <tr>
+                        <th>Email :</th>
+                        <td><input type="email" name="email"></td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><input type="submit" value="add" /></td>
+                    </tr>
+                
             </table>
         </form>
 
