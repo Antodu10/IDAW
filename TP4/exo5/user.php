9a44e2b9c3eca9723ab299f2a20297335bc868f9
Antonin <piat.antonin02@gmail.com>
2023-10-18 11:10:50 +0200

    commit

--- TP4/exo5/user.php
+++ TP4/exo5/user.php
@@ -15,28 +15,37 @@
     catch (PDOException $erreur) {
         echo 'Erreur : '.$erreur->getMessage();
     }
+    $request = $pdo->prepare("select * from users");
+    $request->execute();
+    $resultats=$request->fetchall(PDO::FETCH_OBJ);
 
-
-    if(isset($_POST["name-new"])&&isset($_POST["name-new"])){
-        $request = $pdo->prepare("update users SET name = :name , email = :email WHERE id = :id;");
-        $request->bindParam(":name", $_POST["name-new"], PDO::PARAM_STR);
-        $request->bindParam(":email", $_POST["email-new"], PDO::PARAM_STR);
-        $request->bindParam(":id", $_POST["id-new"], PDO::PARAM_STR);
-        $request->execute();
+    function is_in($ind,$res){
+       
+        $bool=false;
+        foreach ($res as $r){
+            if($ind==$r->id){
+                $bool=true;
+            }
+        }
+        return $bool;
     }
 
     $current_url = $_SERVER['REQUEST_URI'];
 
     switch($_SERVER["REQUEST_METHOD"]){
         case 'GET':
-                $url_parts = parse_url($current_url);
-                $segments = explode('/', $url_parts['path']);
-                $last_segment = end($segments);
-            $request = $pdo->prepare("select * from users where id =:id");
-            $request->bindParam(":id", $last_segment, PDO::PARAM_STR);
-            $request->execute();
-            $resultats=$request->fetch(PDO::FETCH_OBJ);
-            exit(json_encode($resultats));
+            $url_parts = parse_url($current_url);
+            $segments = explode('/', $url_parts['path']);
+            $last_segment = end($segments);
+            if(is_in($last_segment,$resultats)){
+                $request = $pdo->prepare("select * from users where id =:id");
+                $request->bindParam(":id", $last_segment, PDO::PARAM_STR);
+                $request->execute();
+                $resultat=$request->fetch(PDO::FETCH_OBJ);
+                exit(json_encode($resultat));}
+            else{
+               exit(http_response_code(204));
+            };
         case 'POST':
             $data_array = json_decode(file_get_contents('php://input'), true);
             $request = $pdo->prepare("insert into users (name,email) values (:nom,:email)");
@@ -45,18 +54,24 @@
             exit($request->execute());
         case 'DELETE':
             $data_array = json_decode(file_get_contents('php://input'), true);
+            if(is_in($data_array['id'],$resultats)){
             $request = $pdo->prepare("delete from users where id = :id ");
             $request->bindParam(":id", $data_array['id'], PDO::PARAM_STR);
-            exit($request->execute());
+            exit($request->execute());}
+            else{
+                exit(http_response_code(204));
+            };
         case 'PUT':
             $data_array = json_decode(file_get_contents('php://input'), true);
+            if(is_in($data_array['id'],$resultats)){
             $request = $pdo->prepare("update users SET name = :name , email = :email WHERE id = :id;");
             $request->bindParam(":name", $data_array["name"], PDO::PARAM_STR);
             $request->bindParam(":email",$data_array["email"], PDO::PARAM_STR);
             $request->bindParam(":id",$data_array["id"], PDO::PARAM_STR);
-            exit($request->execute());
-
-
+            exit($request->execute());}
+            else{
+                exit(http_response_code(204));
+            };
     }
 
 ?>
